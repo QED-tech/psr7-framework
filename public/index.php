d7e46c55aa27ae1404dc7b6d2025d9e8d6df2124
Vladislav <qedirast@gmail.com>
2021-07-19 22:31:50 +0700

    Added Action Resolver

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Http\Actions\AboutAction;
 use App\Http\Actions\Blog\BlogShowAction;
 use App\Http\Actions\HomeAction;
+use Framework\Http\ActionResolver;
 use Framework\Http\Router\exception\RequestNotMatchedException;
 use Framework\Http\Router\RouteCollection;
 use Framework\Http\Router\Router;
@@ -14,24 +15,24 @@ chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 ### Initialization
+$actionResolver = new ActionResolver();
 $request = ServerRequestFactory::fromGlobals();
 $collection = new RouteCollection();
 
 ## Routes
-$collection->any('home', '/', new HomeAction());
-$collection->any('about', '/about', new AboutAction());
-$collection->any('blog.show', '/blog/{id}', new BlogShowAction(), ['id' => '\d+']);
+$collection->any('home', '/', HomeAction::class);
+$collection->any('about', '/about', AboutAction::class);
+$collection->any('blog.show', '/blog/{id}', BlogShowAction::class, ['id' => '\d+']);
 
 ## Run
 $router = new Router($collection);
 try {
 	$result = $router->match($request);
-	$action = $result->getHandler();
 	foreach ($result->getAttributes() as $attribute => $value) {
 		$request = $request->withAttribute($attribute, $value);
 	}
 	
-	/** @var callable $action */
+	$action = $actionResolver->resolve($result->getHandler());
 	$response = $action($request);
 } catch (RequestNotMatchedException $e) {
 	$response = new Response\JsonResponse(['error' => $e->getMessage()]);
