cf9f7add9918d3fa38d1c78ed1c0b87208c28e22
Vladislav <qedirast@gmail.com>
2021-10-06 14:07:34 +0700

    default template

--- src/App/Http/Actions/AboutAction.php
+++ src/App/Http/Actions/AboutAction.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Actions;
 
-use Laminas\Diactoros\Response\JsonResponse;
+use Framework\Template\TemplateRenderer;
+use Laminas\Diactoros\Response\HtmlResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class AboutAction implements RequestHandlerInterface
 {
+	
+	private TemplateRenderer $renderer;
+	
+	public function __construct(TemplateRenderer $renderer)
+	{
+		$this->renderer = $renderer;
+	}
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new JsonResponse(['page' => 'about page']);
+        return new HtmlResponse($this->renderer->render('app/about'));
     }
 }
